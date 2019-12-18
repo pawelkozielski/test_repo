25a67dff239e8aac440aa5e727a60f2df651f208
kozielsp <pawel.kozielski-romaneczko@contractors.roche.com>
2019-12-18 15:58:28 +0100

    first commit for featureC

--- first_proj.R
+++ first_proj.R
@@ -5,5 +5,6 @@ t <- "third commit"
 z <- "four commit"
 x <- "first commit for featureB"
 e <- "fifth commit"
-print(paste(x, y, t, z, x, e, sep="/"))
+d <- "first commit for featureC"
+print(paste(x, y, t, z, x, e, d, sep="/"))
 
